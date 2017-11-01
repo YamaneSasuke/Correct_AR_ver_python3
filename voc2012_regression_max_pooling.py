ccdedd8bd46fd31210ed038d91a99de5df7ee3d7
Yamane Sasuke <is0240he@ed.ritsumei.ac.jp>
2017-11-01 22:06:53 +0900

    trainer関数を実装

--- voc2012_regression_max_pooling.py
+++ voc2012_regression_max_pooling.py
@@ -18,103 +18,19 @@ import chainer.links as L
 from chainer import cuda, optimizers, Chain, serializers
 from chainer.iterators import MultiprocessIterator
 
-from links import CBR
-import voc2012_regression
+from links import ARConvnet
 from load_datasets import Dataset
 
-
 # ネットワークの定義
-class Convnet_ave(Chain):
+class Bias_sum_pooling(Chain):
     def __init__(self):
-        super(Convnet_ave, self).__init__(
-            cbr1_1=CBR(3, 64, 3, 2, 1),
-            cbr2_1=CBR(64, 128, 3, 2, 1),
-            cbr3_1=CBR(128, 128, 3, 2, 1),
-            cbr4_1=CBR(128, 256, 3, 1, 1),
-            cbr4_2=CBR(256, 256, 3, 2, 1),
-            cbr5_1=CBR(256, 512, 3, 1, 1),
-            cbr5_2=CBR(512, 512, 3, 2, 1),
-
+        super(Bias_sum_pooling, self).__init__(
+            conv=ARConvnet(),
             l1=L.Linear(512, 1)
         )
 
     def __call__(self, X):
-        h = self.cbr1_1(X)
-
-        h = self.cbr2_1(h)
-
-        h = self.cbr3_1(h)
-
-        h = self.cbr4_1(h)
-        h = self.cbr4_2(h)
-
-        h = self.cbr5_1(h)
-        h = self.cbr5_2(h)
-
-        h = F.max_pooling_2d(h, 7)
-        y = self.l1(h)
-        return y
-
-    def bias_ave_pooling(self, h):
-        b_list = []
-        w_i = F.sigmoid(self.xp.sum(h.data, axis=1))
-        for b in range(h.data.shape[0]):
-            c_list = []
-            for c in range(h.data.shape[1]):
-                c_list.append(self.xp.sum(h.data[b][c] * w_i[b]) / self.xp.sum(w_i[b]))
-            b_list.append(self.xp.stack(c_list))
-        new_h = self.xp.stack(b_list)
-        return new_h
-
-    def lossfun(self, X, t):
-        y = self(X)
-        loss = F.mean_squared_error(y, t)
-        return loss
-
-    def loss_ave(self, iterator):
-        losses = []
-        while True:
-            batch = next(iterator)
-            X_batch = batch[0][0]
-            T_batch = batch[0][1]
-            finish = batch[0][2]
-            X_batch = cuda.to_gpu(X_batch)
-            T_batch = cuda.to_gpu(T_batch)
-            with chainer.using_config('train', False), chainer.no_backprop_mode():
-                loss = self.lossfun(X_batch, T_batch)
-            losses.append(cuda.to_cpu(loss.data))
-            if finish is True:
-                break
-        return np.mean(losses)
-
-# ネットワークの定義
-class Convnet_bias_ave(Chain):
-    def __init__(self):
-        super(Convnet_bias_ave, self).__init__(
-            cbr1_1=CBR(3, 64, 3, 2, 1),
-            cbr2_1=CBR(64, 128, 3, 2, 1),
-            cbr3_1=CBR(128, 128, 3, 2, 1),
-            cbr4_1=CBR(128, 256, 3, 1, 1),
-            cbr4_2=CBR(256, 256, 3, 2, 1),
-            cbr5_1=CBR(256, 512, 3, 1, 1),
-            cbr5_2=CBR(512, 512, 3, 2, 1),
-
-            l1=L.Linear(512, 1)
-        )
-
-    def __call__(self, X):
-        h = self.cbr1_1(X)
-
-        h = self.cbr2_1(h)
-
-        h = self.cbr3_1(h)
-
-        h = self.cbr4_1(h)
-        h = self.cbr4_2(h)
-
-        h = self.cbr5_1(h)
-        h = self.cbr5_2(h)
-
+        h = self.conv(X)
         h = self.bias_ave_pooling(h)
         y = self.l1(h)
         return y
@@ -131,7 +47,53 @@ class Convnet_bias_ave(Chain):
         loss = F.mean_squared_error(y, t)
         return loss
 
-    def loss_ave(self, iterator):
+# ネットワークの定義
+class Conv_pooling(Chain):
+    def __init__(self):
+        super(Conv_pooling, self).__init__(
+            conv=ARConvnet(),
+            create_w=L.Convolution2D(512, 1, 7),
+            l1=L.Linear(512, 1)
+        )
+
+    def __call__(self, X):
+        h = self.conv(X)
+        h = self.conv_pooling(h)
+        y = self.l1(h)
+        return y
+
+    def conv_pooling(self, x):
+        w = self.create_w(x)
+        w = F.broadcast_to(w, x.shape)
+        weighted_x = x * w
+        pooled_x = F.sum(weighted_x, axis=(2, 3))
+        return F.relu(pooled_x)
+
+    def lossfun(self, X, t):
+        y = self(X)
+        loss = F.mean_squared_error(y, t)
+        return loss
+
+# ネットワークの定義
+class Ave_pooling(Chain):
+    def __init__(self):
+        super(Ave_pooling, self).__init__(
+            conv=ARConvnet(),
+            l1=L.Linear(512, 1)
+        )
+
+    def __call__(self, X):
+        h = self.conv(X)
+        h = F.average_pooling_2d(h)
+        y = self.l1(h)
+        return y
+
+    def lossfun(self, X, t):
+        y = self(X)
+        loss = F.mean_squared_error(y, t)
+        return loss
+
+def loss_ave(model, iterator):
         losses = []
         while True:
             batch = next(iterator)
@@ -141,27 +103,149 @@ class Convnet_bias_ave(Chain):
             X_batch = cuda.to_gpu(X_batch)
             T_batch = cuda.to_gpu(T_batch)
             with chainer.using_config('train', False), chainer.no_backprop_mode():
-                loss = self.lossfun(X_batch, T_batch)
+                loss = model.lossfun(X_batch, T_batch)
             losses.append(cuda.to_cpu(loss.data))
             if finish is True:
                 break
         return np.mean(losses)
 
+def trainer(file_name, model, optimizer, params):
+    """
+    file_name:
+        type = String
+    model:
+         chainer.Chain class. input = (B, C, H, W). output = (B,). have a lossfun().
+    optimizer:
+        chainer.optimizers.optimizer(learning_rate).setup(model)
+    params:
+        type = list[max_iteration, batch_size, num_train, num_valid, learning_rate, aspect_ratio_max, output_location]
+    """
+    time_start = time.time()
+    train_losses = []
+    valid_losses = []
+    best_epoch = 0
+    best_valid_loss = np.inf
+    max_iteration = params[0]  # 繰り返し回
+    batch_size = params[1]  # ミニバッチサイズ
+    num_train = params[2]  # 学習データ数
+    num_valid = params[3]  # 検証データ数
+    learning_rate = params[4]  # 学習率 test loss順位1
+    aspect_ratio_max = params[5]  # 最大アスペクト比の誤り
+    output_location = params[6]  # 学習結果保存場所
+
+    # 学習結果保存フォルダ作成
+    output_root_dir = os.path.join(output_location, file_name)
+    folder_name = str(time_start)
+    output_root_dir = os.path.join(output_root_dir, folder_name)
+    if os.path.exists(output_root_dir):
+        pass
+    else:
+        os.makedirs(output_root_dir)
+
+    # ファイル名を作成
+    model_filename = str(file_name) + '.npz'
+    loss_filename = 'epoch_loss' + str(time_start) + '.png'
+    model_filename = os.path.join(output_root_dir, model_filename)
+    loss_filename = os.path.join(output_root_dir, loss_filename)
+
+    # バッチサイズ計算
+    num_batches_train = int(num_train / batch_size)
+    # stream作成
+    train_data = Dataset(batch_size, 0, 16500, train=True)
+    valid_data = Dataset(batch_size, 16500, 17000, train=False)
+    test_data = Dataset(batch_size, 1700, 17200, train=False)
+    train_ite = MultiprocessIterator(train_data, 1, n_processes=1)
+    valid_ite = MultiprocessIterator(valid_data, 1, n_processes=1)
+    test_ite = MultiprocessIterator(test_data, 1, n_processes=1)
+
+    time_origin = time.time()
+    try:
+        for epoch in range(max_iteration):
+            time_begin = time.time()
+            losses = []
+            for i in tqdm.tqdm(range(num_batches_train)):
+                batch = next(train_ite)
+                X_batch = batch[0][0]
+                T_batch = batch[0][1]
+                finish = batch[0][2]
+                X_batch = cuda.to_gpu(X_batch)
+                T_batch = cuda.to_gpu(T_batch)
+                # 勾配を初期化
+                model.cleargrads()
+                with chainer.using_config('train', True):
+                    # 順伝播を計算し、誤差と精度を取得
+                    loss = model.lossfun(X_batch, T_batch)
+                    # 逆伝搬を計算
+                    loss.backward()
+                optimizer.update()
+                losses.append(cuda.to_cpu(loss.data))
+                if finish is True:
+                    break
+
+            time_end = time.time()
+            epoch_time = time_end - time_begin
+            total_time = time_end - time_origin
+            train_losses.append(np.mean(losses))
+
+            valid_loss = loss_ave(model, valid_ite)
+            valid_losses.append(valid_loss)
+
+            if valid_loss < best_valid_loss:
+                best_valid_loss = valid_loss
+                best_epoch = epoch
+                best_model = copy.deepcopy(model)
+
+            # 訓練データでの結果を表示
+            print()
+            print(file_name)
+            print("epoch:", epoch+1)
+            print("time", epoch_time, "(", total_time, ")")
+            print("loss[train]:", train_losses[epoch])
+            print("loss[valid]:", valid_losses[epoch])
+            print("best epoch", best_epoch)
+            print("loss_best:", best_valid_loss)
+
+            plt.plot(train_losses)
+            plt.plot(valid_losses)
+            plt.ylim(0, 0.5)
+            plt.title("loss")
+            plt.legend(["train", "valid"], bbox_to_anchor=(1.3, 1), loc="upper right")
+            plt.grid()
+            plt.show()
+
+    except KeyboardInterrupt:
+        print("割り込み停止が実行されました")
+
+    train_ite.finalize()
+    valid_ite.finalize()
+    test_ite.finalize()
+
+    plt.plot(train_losses)
+    plt.plot(valid_losses)
+    plt.ylim(0, 0.5)
+    plt.title("loss")
+    plt.legend(["train", "valid"], bbox_to_anchor=(1.3, 1), loc="upper right")
+    plt.grid()
+    plt.savefig(loss_filename)
+    plt.show()
+
+    serializers.save_npz(model_filename, best_model)
+
+    print()
+    print('max_iteration:', max_iteration)
+    print('batch_size:', batch_size)
+    print('train_size', num_train)
+    print('valid_size', num_valid)
+    print('aspect_ratio_max', aspect_ratio_max)
+    print('learning_rate:', learning_rate)
+    print('best_epoch:', best_epoch)
+    print("best_valid_loss:", best_valid_loss)
+
+    return train_losses, valid_losses, best_model
 
 if __name__ == '__main__':
     __spec__ = None
     file_name = os.path.splitext(os.path.basename(__file__))[0]
-    time_start = time.time()
-    epoch_loss1 = []
-    epoch_loss2 = []
-    epoch_loss3 = []
-    epoch_valid_loss1 = []
-    epoch_valid_loss2 = []
-    epoch_valid_loss3 = []
-    loss_valid_best1 = np.inf
-    loss_valid_best2 = np.inf
-    loss_valid_best3 = np.inf
-#    t_loss = []
 
     # 超パラメータ
     max_iteration = 100000  # 繰り返し回数
@@ -172,150 +256,15 @@ if __name__ == '__main__':
     aspect_ratio_max = 4.0  # 最大アスペクト比の誤り
     # 学習結果保存場所
     output_location = r'C:\Users\yamane\OneDrive\M1\correct_aspect_ratio'
-    # 学習結果保存フォルダ作成
-    output_root_dir = os.path.join(output_location, file_name)
-    folder_name = str(time_start) + '_asp_max_' + str(aspect_ratio_max)
-    output_root_dir = os.path.join(output_root_dir, folder_name)
-    if os.path.exists(output_root_dir):
-        pass
-    else:
-        os.makedirs(output_root_dir)
-    # ファイル名を作成
-#    model_filename1 = str(file_name) + '_ave' + '.npz'
-    model_filename2 = str(file_name) + '_bias_ave' + '.npz'
-    loss_filename = 'epoch_loss' + str(time_start) + '.png'
-#    t_dis_filename = 't_distance' + str(time_start) + '.png'
-#    model_filename1 = os.path.join(output_root_dir, model_filename1)
-    model_filename2 = os.path.join(output_root_dir, model_filename2)
-    loss_filename = os.path.join(output_root_dir, loss_filename)
-#    t_dis_filename = os.path.join(output_root_dir, t_dis_filename)
-    # バッチサイズ計算
-    num_batches_train = int(num_train / batch_size)
-    num_batches_valid = int(num_valid / batch_size)
-    # stream作成
-    train_data = Dataset(batch_size, 0, 16500, train=True)
-    valid_data = Dataset(batch_size, 16500, 17000, train=False)
-    test_data = Dataset(batch_size, 1700, 17200, train=False)
-    train_ite = MultiprocessIterator(train_data, 1, n_processes=1)
-    valid_ite = MultiprocessIterator(valid_data, 1, n_processes=1)
-    test_ite = MultiprocessIterator(test_data, 1, n_processes=1)
     # モデル読み込み
-    model1 = Convnet_ave().to_gpu()
-    model2 = Convnet_bias_ave().to_gpu()
+    model = Conv_pooling().to_gpu()
+
     # Optimizerの設定
-#    optimizer1 = optimizers.Adam(learning_rate)
-#    optimizer1.setup(model1)
-    optimizer2 = optimizers.Adam(learning_rate)
-    optimizer2.setup(model2)
+    optimizer = optimizers.Adam(learning_rate)
+    optimizer.setup(model)
 
-    time_origin = time.time()
-    try:
-        for epoch in range(max_iteration):
-            time_begin = time.time()
-#            losses1 = []
-            losses2 = []
-            for i in tqdm.tqdm(range(num_batches_train)):
-                batch = next(train_ite)
-                X_batch = batch[0][0]
-                T_batch = batch[0][1]
-                finish = batch[0][2]
-                X_batch = cuda.to_gpu(X_batch)
-                T_batch = cuda.to_gpu(T_batch)
-                # 勾配を初期化
-#                model1.cleargrads()
-                model2.cleargrads()
-                with chainer.using_config('train', True):
-                    # 順伝播を計算し、誤差と精度を取得
-#                    loss1 = model1.lossfun(X_batch, T_batch)
-                    loss2 = model2.lossfun(X_batch, T_batch)
-                    # 逆伝搬を計算
-#                    loss1.backward()
-                    loss2.backward()
-#                optimizer1.update()
-                optimizer2.update()
-#                losses1.append(cuda.to_cpu(loss1.data))
-                losses2.append(cuda.to_cpu(loss2.data))
-                if finish is True:
-                    break
-
-            time_end = time.time()
-            epoch_time = time_end - time_begin
-            total_time = time_end - time_origin
-#            epoch_loss1.append(np.mean(losses1))
-            epoch_loss2.append(np.mean(losses2))
-
-#            loss_valid1 = model1.loss_ave(valid_ite)
-            loss_valid2 = model2.loss_ave(valid_ite)
-#            epoch_valid_loss1.append(loss_valid1)
-            epoch_valid_loss2.append(loss_valid2)
-
-#            if loss_valid1 < loss_valid_best1:
-#                loss_valid_best1 = loss_valid1
-#                epoch__loss_best1 = epoch
-#                model_best1 = copy.deepcopy(model1)
-
-            if loss_valid2 < loss_valid_best2:
-                loss_valid_best2 = loss_valid2
-                epoch__loss_best2 = epoch
-                model_best2 = copy.deepcopy(model2)
-
-            # 訓練データでの結果を表示
-            print()
-            print("voc2012_regression_max_pooling.py")
-            print("epoch:", epoch+1)
-            print("time", epoch_time, "(", total_time, ")")
-#            print("loss_ave[train]:", epoch_loss1[epoch])
-            print("loss_bias_ave[train]:", epoch_loss2[epoch])
-#            print("loss_ave[valid]:", loss_valid1)
-            print("loss_bias_ave[valid]:", loss_valid2)
-#            print("loss[valid_best]:", loss_valid_best)
-#            print("epoch[valid_best]:", epoch__loss_best)
-
-#            if (epoch % 10) == 0:
-#            plt.plot(epoch_loss1)
-            plt.plot(epoch_loss2)
-#            plt.plot(epoch_valid_loss1)
-            plt.plot(epoch_valid_loss2)
-            plt.ylim(0, 0.5)
-            plt.title("loss")
-            plt.legend(["train_bias_ave", "valid_bias_ave"], bbox_to_anchor=(1.3, 1), loc="upper right")
-            plt.grid()
-            plt.show()
-
-            # 検証用のデータを取得
-#            test_batch = next(test_ite)
-#            X_valid = test_batch[0][0]
-#            T_valid = test_batch[0][1]
-#            t_loss = voc2012_regression.test_output(model_best, X_valid,
-#                                                    T_valid, t_loss)
-
-    except KeyboardInterrupt:
-        print("割り込み停止が実行されました")
-
-    train_ite.finalize()
-    valid_ite.finalize()
-    test_ite.finalize()
-
-#    plt.plot(epoch_loss1)
-    plt.plot(epoch_loss2)
-#    plt.plot(epoch_valid_loss1)
-    plt.plot(epoch_valid_loss2)
-    plt.ylim(0, 0.5)
-    plt.title("loss")
-    plt.legend(["train_bias_ave", "valid_bias_ave"], bbox_to_anchor=(1.3, 1), loc="upper right")
-    plt.grid()
-    plt.savefig(loss_filename)
-    plt.show()
-
-#    serializers.save_npz(model_filename1, model_best1)
-
-    serializers.save_npz(model_filename2, model_best2)
-
-    print('max_iteration:', max_iteration)
-    print('batch_size:', batch_size)
-    print('train_size', num_train)
-    print('valid_size', num_valid)
-    print('aspect_ratio_max', aspect_ratio_max)
-    print('learning_rate:', learning_rate)
-#    print("loss_valid_best_ave:", loss_valid_best1)
-    print("loss_valid_best_bias_ave:", loss_valid_best2)
+    params = [max_iteration, batch_size, num_train, num_valid, learning_rate,
+              aspect_ratio_max, output_location]
+    # モデルの学習
+    train_loss, valid_loss, best_model = trainer(
+            file_name, model, optimizer, params)
